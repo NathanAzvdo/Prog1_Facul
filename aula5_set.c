d7461c0708c949cc5cef6805cc455e70d8c22c80
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-09-05 19:25:21 -0300

    first class

---
+++ aula5_set.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+    int i, numero;
+    int soma=0;
+    printf("Digite um número:");
+    scanf("%d", &numero);
+    for(i=1; i<=numero;i++){
+        soma+=i;
+    }
+    printf("Soma:%d\n", soma);
+
+}
