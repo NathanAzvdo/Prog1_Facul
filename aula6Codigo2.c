492c43c9a7edc442cd71b9dca8c087d40e060055
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-09-27 19:20:14 -0300

    ldjsfj

--- aula6Codigo2.c
+++ aula6Codigo2.c
@@ -7,7 +7,11 @@ main(){
     for(i=10;i<100;i++){
         if(i%2==1){
             printf("%d\n", i);
-            soma+=i;
+            somaI+=i;
+        }
+        if(i%2==0){
+            printf("%d\n", i);
+            somaP+=i;
         }
     }
     printf("soma:%d\n", soma);
